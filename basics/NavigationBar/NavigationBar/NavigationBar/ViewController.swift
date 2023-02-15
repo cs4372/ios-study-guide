79f411357232934f53b3f9c8b63d60e89bb9d3b6
cs4372 <cshing4372@gmail.com>
2023-02-15 00:27:03 -0500

    Add Navigation Bar Notes

---
+++ basics/NavigationBar/NavigationBar/NavigationBar/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by Catherine Shing on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addItem(_ sender: Any) {
+        print("print")
+    }
+    
+}
+
