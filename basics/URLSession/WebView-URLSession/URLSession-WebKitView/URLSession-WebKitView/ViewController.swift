bb3845996aacb2e1676e44bc45176ea985057522
cs4372 <cshing4372@gmail.com>
2023-02-16 23:49:21 -0500

    Add URLSession notes

---
+++ basics/URLSession/WebView-URLSession/URLSession-WebKitView/URLSession-WebKitView/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  URLSession-WebKitView
+//
+//  Created by Catherine Shing on 2/16/23.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        let urlString = "http://www.stackoverflow.com"
+//        let request = URLRequest(url: URL(string: urlString)!)
+//        self.webView.load(request)
+//        self.webView.loadHTMLString("<h1>Hello there<h1>", baseURL: nil)
+        
+        // Get data from the web
+        if let url = URL(string: "https://www.google.com") {
+             URLSession.shared.dataTask(with: url) {
+                data, response, error in
+                guard let data = data else { return }
+                print(String(data: data, encoding: .utf8)!)
+                DispatchQueue.main.sync(execute: {
+                    //Update UI
+                })
+            }
+    }
+}
+
