dad7568280a7c12e14ba9c9a4fc9b60dbacd175e
cs4372 <cshing4372@gmail.com>
2023-08-28 00:18:32 -0400

    responsive UI

--- interview-prep/Playgrounds/ResponsiveUI.playground/Contents.swift
+++ interview-prep/Playgrounds/ResponsiveUI.playground/Contents.swift
@@ -79,6 +79,7 @@ import PlaygroundSupport
 //}
 
 // Build a simple tableview with static data
+// Selecting table cell should navigate to controller and display text name
 
 class TableViewExampleController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -113,6 +114,26 @@ class TableViewExampleController: UIViewController, UITableViewDataSource, UITab
           return cell
      }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let selectedName = names[indexPath.row]
+        print("selectedName", selectedName)
+        let viewNameController = ViewNameController()
+        viewNameController.selectedName = selectedName
+        let navigationController = UINavigationController(rootViewController: viewNameController)
+        
+        navigationController.modalPresentationStyle = .fullScreen
+        
+        /* Xcode
+         viewNameController.selectedName = selectedName
+          
+        navigationController?.pushViewController(viewNameController, animated: true)
+         */
+        
+        present(navigationController, animated: true, completion: nil)
+    }
+    
     func setupContraints() {
         self.view.addSubview(self.tableView)
 
@@ -125,6 +146,42 @@ class TableViewExampleController: UIViewController, UITableViewDataSource, UITab
     }
 }
 
-//PlaygroundPage.current.liveView = ExampleController()
+class ViewNameController: UIViewController {
+    var selectedName: String?
+    
+    private let nameLabel: UILabel = {
+         let label = UILabel()
+         label.textColor = .label
+         label.textAlignment = .center
+         label.font = .systemFont(ofSize: 16, weight: .regular)
+         label.text = "Unknown"
+         return label
+     }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonTapped))
+                
+        if let selectedName = selectedName {
+            nameLabel.text = selectedName
+            nameLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            view.addSubview(nameLabel)
+            
+            NSLayoutConstraint.activate([
+                nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+        }
+    }
+    // doesn't work in playground
+    
+    @objc func backButtonTapped() {
+         navigationController?.popViewController(animated: true)
+     }
+}
 
 PlaygroundPage.current.liveView = TableViewExampleController()
+
+//PlaygroundPage.current.liveView = ExampleController()
